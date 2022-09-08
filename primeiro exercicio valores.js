6313fc98222301212c34cd480d064746d64f1651
KaiqueBourguignon <kaiquegeneration@gmail.com>
2022-09-08 16:45:17 -0300

    mensagem

---
+++ primeiro exercicio valores.js
@@ -0,0 +1,17 @@
+const prompt = require("prompt-sync")();
+
+const nome = prompt ("escreva seu nome");
+console.log(`digite seu nome : ${nome}` );
+
+
+const peso = prompt ("fale seu peso");
+console.log(`seu peso e : ${peso}` );
+
+
+const altura = prompt ("digite sua alta");
+console.log(`sua altura é :  ${altura}` );
+
+const diabetico = prompt ("Digite seu seu peso");
+console.log(`informação registrada `)
+
+
